524797a924462e25880dddb61984ee0028e772e3
NurAhissami <nurahi10@gmail.com>
2021-06-08 10:23:38 +0200

    Add final Examn

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import data from '../data/pokemons.json';
 function App() {
   return (
     <div className="App">
-      <h2>Pokemon LIST</h2>
+      <h2 className="Title">Pokemon List</h2>
       <PokeList pokemons={data} />
     </div>
   );
